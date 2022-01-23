dfa6b3f4e56c9259fa6d7938d71bde1667818fb0
Vaishnavi7279 <srivastava93vaishnavi@gmail.com>
2022-01-23 13:40:50 +0530

    Updates

---
+++ Miscellaneous Solutions/7.1-23rdJan-LargestNumber.cpp
@@ -0,0 +1,28 @@
+//LeetCode Problem
+//Largest Number
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    public:
+    string largestNumber(vector<int> & nums) 
+      {
+        sort(nums.begin(),nums.end(),[&](int &a,int &b)
+            {
+              string s1=to_string(a);
+              string s2=to_string(b);
+              return s1+s2>s2+s1;
+            }
+        string ans="";
+        for(int i=0;i<nums.size();i++)
+           {
+             string s=to_string(nums[i]);
+             ans += s;
+           }
+        if(ans[0]=='0')
+          {
+             return "0";
+          }
+        return ans; 
+      };
+}
